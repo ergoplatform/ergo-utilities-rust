70207bb2fce197f978c45559623be93bf0cc4ecf
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2020-10-02 13:17:41 +0200

    Further exports

--- ergo-protocol-framework/src/lib.rs
+++ ergo-protocol-framework/src/lib.rs
@@ -5,3 +5,5 @@ pub mod stage;
 pub use stage::{Stage, StageType, BoxVerificationError, Result};
 pub use protocol::Protocol;
 pub use ergo_offchain_utilities::P2SAddressString;
+pub use ergo_lib::chain::ergo_box::ErgoBox;
+pub use ergo_lib::ast::Constant;
