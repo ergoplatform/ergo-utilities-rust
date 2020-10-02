8027c9a6e5745691a1bb7c2ac6e51d4560040028
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2020-10-02 13:16:24 +0200

    Further exports from stage

--- ergo-protocol-framework/src/lib.rs
+++ ergo-protocol-framework/src/lib.rs
@@ -2,6 +2,6 @@ pub mod predicated_boxes;
 pub mod protocol;
 pub mod stage;
 
-pub use stage::{Stage, StageType};
+pub use stage::{Stage, StageType, BoxVerificationError, Result};
 pub use protocol::Protocol;
 pub use ergo_offchain_utilities::P2SAddressString;
