e7b43789fd2451c66cecd1fc0d04818443a8efab
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2020-10-06 13:48:46 +0200

    Created `OracleBoxLong`

--- ergo-protocol-framework/src/predicated_boxes.rs
+++ ergo-protocol-framework/src/predicated_boxes.rs
@@ -6,6 +6,7 @@
 /// which are enforced by the predicates inside of each predicated
 /// box.
 use crate::stage::StageType;
+pub use ergo_lib::ast::ConstantVal;
 use ergo_lib::chain::ergo_box::ErgoBox;
 use thiserror::Error;
 
@@ -21,6 +22,8 @@ pub enum BoxVerificationError {
     InvalidTokens(String),
     #[error("The provided `ErgoBox` did not pass the verification predicate because of a problem with the values within the registers of the box: {0}")]
     InvalidRegisters(String),
+    #[error("The provided `ErgoBox` is not a valid Oracle Box: {0}")]
+    InvalidOracleBox(String),
     #[error("{0}")]
     OtherError(String),
 }
@@ -82,6 +85,65 @@ impl ErgsBox {
         return Ok(ErgsBox {
             ergo_box: b.clone(),
             predicate: box_with_ergs_predicate,
+        });
+    }
+}
+
+/// A predicated box which indicates it is an
+/// oracle box which stores a `Long` integer datapoint inside of R4.
+/// This may be an Oracle Pool box, or any other kind of oracle box.
+/// This predicated box automatically extracts the long datapoint from the
+/// box and exposes it as a public field to be easily used.
+pub struct OracleBoxLong {
+    pub ergo_box: ErgoBox,
+    pub predicate: fn(&ErgoBox) -> Result<()>,
+    pub datapoint: i64,
+}
+/// Extracts a Long out of register R4 of the provided `ErgoBox`.
+/// Does error-checking along the way.
+fn extract_long_datapoint(b: &ErgoBox) -> Result<i64> {
+    let registers = b.additional_registers.get_ordered_values();
+    if registers.len() < 1 {
+        return Err(BoxVerificationError::InvalidOracleBox(
+            "No datapoint in R4.".to_string(),
+        ));
+    } else {
+        // Match on the ConstantVal::Long of Register R4
+        match registers[0].v {
+            ConstantVal::Long(i) => return Ok(i),
+            _ => {
+                return Err(BoxVerificationError::InvalidOracleBox(
+                    "Value in R4 is not a Long.".to_string(),
+                ))
+            }
+        };
+    }
+}
+/// Predicate to check that a box has a valid Long datapoint in R4.
+fn oracle_box_predicate(b: &ErgoBox) -> Result<()> {
+    // Using `?` to verify that a valid Long datapoint was extracted.
+    // If it failed, it will push the error upwards.
+    extract_long_datapoint(b)?;
+    Ok(())
+}
+impl PredicatedBox for OracleBoxLong {
+    /// Empty predicate that always passes.
+    fn predicate(&self) -> fn(&ErgoBox) -> Result<()> {
+        oracle_box_predicate
+    }
+    fn get_box(&self) -> ErgoBox {
+        self.ergo_box.clone()
+    }
+}
+impl OracleBoxLong {
+    /// Create a new `NoPredicateBox`
+    pub fn new(b: &ErgoBox) -> Result<OracleBoxLong> {
+        // This automatically does error/predicate checking while extracting.
+        let datapoint = extract_long_datapoint(b)?;
+        return Ok(OracleBoxLong {
+            ergo_box: b.clone(),
+            predicate: oracle_box_predicate,
+            datapoint: datapoint,
         });
     }
 }
