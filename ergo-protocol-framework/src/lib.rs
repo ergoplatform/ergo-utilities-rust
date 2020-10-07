8a14477063553f3631d3616a10c9892a36266ca3
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2020-10-07 13:52:43 +0200

    Export further types

--- ergo-protocol-framework/src/lib.rs
+++ ergo-protocol-framework/src/lib.rs
@@ -4,7 +4,7 @@ pub mod stage;
 
 pub use ergo_lib::ast::Constant;
 pub use ergo_lib::chain::ergo_box::ErgoBox;
-pub use ergo_offchain_utilities::{NanoErg, P2SAddressString};
+pub use ergo_offchain_utilities::{BlockDuration, BlockHeight, NanoErg, P2SAddressString};
 pub use predicated_boxes::{
     BoxVerificationError, ErgsBox, OracleBoxLong, PredicatedBox, Result, StageBox,
 };
