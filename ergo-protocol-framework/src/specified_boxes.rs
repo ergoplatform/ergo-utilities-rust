de1b44fc4a43efc054646e2a9c895117bc35a233
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2020-11-13 18:05:42 +0100

    Finished ErgsBox impl

--- ergo-protocol-framework/src/specified_boxes.rs
+++ ergo-protocol-framework/src/specified_boxes.rs
@@ -17,16 +17,16 @@ pub struct ErgsBox {
     ergo_box: ErgoBox,
 }
 
-// /// WASM ErgsBox Methods
-// #[wasm_bindgen]
-// impl ErgsBox {
-//     /// Create a new `ErgsBox`
-//     #[wasm_bindgen(constructor)]
-//     pub fn w_new(wb: WErgoBox) -> Result<ErgsBox, JsValue> {
-//         let b: ErgoBox = wb.into();
-//         ErgsBox::new(&b).map_err(|e| JsValue::from_str(&format! {"{:?}", e}))
-//     }
-// }
+/// WASM ErgsBox Methods
+#[wasm_bindgen]
+impl ErgsBox {
+    /// Create a new `ErgsBox`
+    #[wasm_bindgen(constructor)]
+    pub fn w_new(wb: WErgoBox) -> std::result::Result<ErgsBox, JsValue> {
+        let b: ErgoBox = wb.into();
+        ErgsBox::new(&b).map_err(|e| JsValue::from_str(&format! {"{:?}", e}))
+    }
+}
 
 impl WrappedBox for ErgsBox {
     fn get_box(&self) -> ErgoBox {
@@ -73,16 +73,98 @@ impl ErgsBox {
     }
 }
 
-// PredicatedBox ErgsBox Methods
-// impl PredicatedBox for ErgsBox {
-//     /// Predicate to check that a box has more than `1000000` nanoErgs
+// /// A predicated box which indicates it is an
+// /// oracle box which stores a `Long` integer datapoint inside of R4.
+// /// This may be an Oracle Pool box, or any other kind of oracle box.
+// /// This predicated box automatically extracts the long datapoint from the
+// /// box and exposes it as a public field to be easily used.
+// /// The predicate also checks that the box has a single type of Token
+// /// and said token has a value of 1. (Checking that it has an NFT)
+// #[wasm_bindgen]
+// pub struct OracleBoxLong {
+//     ergo_box: ErgoBox,
+//     pub datapoint: i64,
+//     /// The token id of the oracle's NFT
+//     nft_id: String,
+// }
+
+// /// WASM OracleBoxLong Methods
+// #[wasm_bindgen]
+// impl OracleBoxLong {
+//     #[wasm_bindgen(constructor)]
+//     pub fn w_new(wb: WErgoBox) -> Result<OracleBoxLong, JsValue> {
+//         let b: ErgoBox = wb.into();
+//         OracleBoxLong::new(&b).map_err(|e| JsValue::from_str(&format! {"{:?}", e}))
+//     }
+
+//     #[wasm_bindgen(getter)]
+//     pub fn nft_id(&self) -> String {
+//         self.nft_id.clone()
+//     }
+// }
+
+// impl WrappedBox for OracleBoxLong {
+//     fn get_box(&self) -> ErgoBox {
+//         self.ergo_box.clone()
+//     }
+// }
+
+// /// PredicatedBox OracleBoxLong Methods
+// impl PredicatedBox for OracleBoxLong {
+//     /// Predicate to check that a box has a valid Long datapoint in R4.
 //     fn predicate(b: &ErgoBox) -> Result<(), BoxVerificationError> {
-//         if b.value.as_u64().clone() >= 1000000 {
-//             Ok(())
+//         // Using `?` to verify that a valid Long datapoint was extracted.
+//         // If it failed, it will push the error upwards.
+//         OracleBoxLong::extract_long_datapoint(b)?;
+
+//         // Check only a single token type is held in the box
+//         if b.tokens.len() != 1 {
+//             return Err(BoxVerificationError::InvalidTokens(
+//                 "The oracle box is required to only hold a single NFT token.".to_string(),
+//             ));
+//         }
+//         // Check that said single type of token is value == 1. (Aka is an NFT)
+//         if u64::from(b.tokens[0].amount) != 1 {
+//             return Err(BoxVerificationError::InvalidTokens(
+//                 "The oracle box is required to only hold a single NFT token.".to_string(),
+//             ));
+//         }
+//         Ok(())
+//     }
+// }
+
+// /// Rust OracleBoxLong Methods
+// impl OracleBoxLong {
+//     // Create a new `OracleBoxLong`
+//     pub fn new(b: &ErgoBox) -> Result<OracleBoxLong, BoxVerificationError> {
+//         // Error Checking
+//         OracleBoxLong::predicate(&b)?;
+//         let datapoint = OracleBoxLong::extract_long_datapoint(&b)?;
+//         return Ok(OracleBoxLong {
+//             ergo_box: b.clone(),
+//             datapoint: datapoint,
+//             nft_id: b.tokens[0].token_id.0.clone().into(),
+//         });
+//     }
+
+//     /// Extracts a Long out of register R4 of the provided `ErgoBox`.
+//     /// Does error-checking along the way.
+//     fn extract_long_datapoint(b: &ErgoBox) -> Result<i64, BoxVerificationError> {
+//         let registers = b.additional_registers.get_ordered_values();
+//         if registers.len() < 1 {
+//             return Err(BoxVerificationError::InvalidOracleBox(
+//                 "No datapoint in R4.".to_string(),
+//             ));
 //         } else {
-//             Err(BoxVerificationError::InvalidErgsValue(
-//                 "ErgoBox did not have more than 999999 nanoErgs inside.".to_string(),
-//             ))
+//             // Match on the ConstantVal::Long of Register R4
+//             match registers[0].v {
+//                 ConstantVal::Long(i) => return Ok(i),
+//                 _ => {
+//                     return Err(BoxVerificationError::InvalidOracleBox(
+//                         "Value in R4 is not a Long.".to_string(),
+//                     ))
+//                 }
+//             };
 //         }
 //     }
 // }
