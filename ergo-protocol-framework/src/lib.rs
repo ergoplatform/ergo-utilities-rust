2e4d165e7f69db2d86a1a3c65891a3ef9505f9d2
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2020-11-16 11:41:59 +0100

    Updated exports

--- ergo-protocol-framework/src/lib.rs
+++ ergo-protocol-framework/src/lib.rs
@@ -4,3 +4,10 @@ pub mod error;
 pub mod output_builders;
 pub mod specified_boxes;
 pub mod tx_creation;
+
+pub use box_spec::{BoxSpec, TokenSpec};
+pub use box_traits::{SpecifiedBox, WrappedBox};
+pub use error::{ProtocolFrameworkError, Result};
+pub use output_builders::{ChangeBox, TokensChangeBox, TxFeeBox};
+pub use specified_boxes::{ErgsBox, OracleBoxLong};
+pub use tx_creation::{create_candidate, find_and_sum_other_tokens};
