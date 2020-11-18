84c0c8e12a501cea4688f4c674a8c70967356937
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2020-11-18 17:28:46 +0100

    Significant progess on example/tutorial

--- ergo-protocol-framework/tutorials/Math_Bounty/math-bounty-lib/src/lib.rs
+++ ergo-protocol-framework/tutorials/Math_Bounty/math-bounty-lib/src/lib.rs
@@ -5,3 +5,27 @@ use ergo_protocol_framework::*;
 pub struct MathProblemBox {
     ergo_box: ErgoBox,
 }
+
+impl SpecifiedBox for MathProblemBox {
+    fn box_spec() -> BoxSpec {
+        let address = Some("94hWSMqgxHtRNEWoKrJFGVNQEYX34zfX68FNxWr".to_string());
+        BoxSpec::new(address, None, vec![], vec![])
+    }
+}
+
+impl MathProblemBox {
+    pub fn new(ergo_box: &ErgoBox) -> Option<MathProblemBox> {
+        // Using the automatically implemented `verify_box` method
+        // from the `BoxSpec` to verify the `ErgoBox` is a valid
+        // `MathProblemBox`.
+        Self::box_spec().verify_box(ergo_box).ok()?;
+
+        // Creating the `MathProblemBox`
+        let math_problem_box = MathProblemBox {
+            ergo_box: ergo_box.clone(),
+        };
+
+        // Returning the `MathProblemBox`
+        Some(math_problem_box)
+    }
+}
