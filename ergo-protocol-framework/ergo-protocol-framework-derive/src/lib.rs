f8c17eacb32427739066dcdcf364260b25a24c4f
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2020-11-19 18:38:08 +0100

    Implemented `SpecBox` proc derive macro

--- ergo-protocol-framework/ergo-protocol-framework-derive/src/lib.rs
+++ ergo-protocol-framework/ergo-protocol-framework-derive/src/lib.rs
@@ -25,3 +25,28 @@ fn impl_wrapped_box(ast: &syn::DeriveInput) -> TokenStream {
     };
     gen.into()
 }
+
+#[proc_macro_derive(SpecBox)]
+pub fn specified_box_derive(input: TokenStream) -> TokenStream {
+    // Construct a representation of Rust code as a syntax tree
+    // that we can manipulate
+    let ast = syn::parse(input).unwrap();
+
+    // Build the trait implementation
+    impl_specified_box(&ast)
+}
+
+fn impl_specified_box(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl SpecifiedBox for #name {
+            pub fn new(b: &ErgoBox) -> std::result::Result<#name, ergo_protocol_framework::ProtocolFrameworkError> {
+                Self::box_spec().verify_box(&b)?;
+                return Ok(#name {
+                    ergo_box: b.clone(),
+                });
+            }
+        }
+    };
+    gen.into()
+}
