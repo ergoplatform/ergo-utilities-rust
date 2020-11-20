e2ba5e2143b55ad218fb53359f18aa1374c4ef26
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2020-11-20 09:39:23 +0100

    Exposing further types

--- ergo-protocol-framework/src/lib.rs
+++ ergo-protocol-framework/src/lib.rs
@@ -5,13 +5,17 @@ pub mod output_builders;
 pub mod specified_boxes;
 pub mod tx_creation;
 
-pub use box_spec::{BoxSpec, TokenSpec};
+pub use box_spec::{BoxSpec, RegisterSpec, TokenSpec};
 pub use box_traits::{SpecifiedBox, WrappedBox};
 pub use ergo_lib::ast::constant::Constant;
 pub use ergo_lib::chain::ergo_box::ErgoBox;
 pub use ergo_lib::chain::transaction::unsigned::UnsignedTransaction;
 pub use ergo_lib::ergo_tree::ErgoTree;
 pub use ergo_lib::types::stype::SType;
+pub use ergo_offchain_utilities::{
+    BlockDuration, BlockHeight, ErgoAddressString, NanoErg, P2PKAddressString, P2SAddressString,
+    ScanID, TokenID, TxId,
+};
 pub use ergo_protocol_framework_derive::{SpecBox, WrapBox};
 pub use error::{ProtocolFrameworkError, Result};
 pub use output_builders::{ChangeBox, TokensChangeBox, TxFeeBox};
