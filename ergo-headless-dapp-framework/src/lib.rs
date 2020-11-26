d365bfa31ce0fbc49a684d27f87094b8cbfc72f2
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2020-11-26 10:08:48 +0100

    Exporting ErgUsdOraclePoolBox

--- ergo-headless-dapp-framework/src/lib.rs
+++ ergo-headless-dapp-framework/src/lib.rs
@@ -17,7 +17,7 @@ pub use ergo_lib::ergo_tree::ErgoTree;
 pub use ergo_lib::types::stype::SType;
 pub use error::{HeadlessDappError, Result};
 pub use output_builders::{ChangeBox, TokensChangeBox, TxFeeBox};
-pub use specified_boxes::ErgsBox;
+pub use specified_boxes::{ErgUsdOraclePoolBox, ErgsBox};
 pub use tx_creation::{create_candidate, find_and_sum_other_tokens};
 
 /// A Base58 encoded String of an Ergo address. Can be either P2PK or P2S.
